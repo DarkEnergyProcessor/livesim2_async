9193f907ae9ef1b1e8c43b4adeabecf1453f00c7
Miku AuahDark <auahdark687291@gmail.com>
2018-08-29 23:29:18 +0800

    Updated Lily to v2.0.11
    Fixed image font loading

--- game/live/ui/sif.lua
+++ game/live/ui/sif.lua
@@ -5,13 +5,20 @@
 local love = require("love")
 local Luaoop = require("libs.Luaoop")
 local assetCache = require("asset_cache")
+local lily = require("libs.lily")
+local async = require("async")
 local color = require("color")
 local uibase = require("game.live.uibase")
 local sifui = Luaoop.class("livesim2.SIFLiveUI", uibase)
 
 function sifui:__construct()
 	-- as per uibase definition, constructor can use
-	-- any asynchronous operation
+	-- any asynchronous operation (async lib)
+	local fontImageDataList = lily.loadMulti({
+		{lily.newImageData, "assets/image/live/score_num/score.png"},
+		{lily.newImageData, "assets/image/live/score_num/addscore.png"},
+		{lily.newImageData, "assets/image/live/hp_num.png"}
+	})
 	self.images = assetCache.loadMultipleImages({
 		-- live header
 		"assets/image/live/live_header.png", -- 1
@@ -26,27 +33,24 @@ function sifui:__construct()
 		"assets/image/live/l_gauge_17.png",
 		-- scoring
 		"assets/image/live/l_etc_46.png", -- 10
-		"assets/image/live/score_num/score.png",
-		"assets/image/live/score_num/addscore.png",
 		-- stamina
-		"assets/image/live/live_gauge_02_01.png", -- 13
+		"assets/image/live/live_gauge_02_01.png", -- 11
 		"assets/image/live/live_gauge_02_02.png",
 		"assets/image/live/live_gauge_02_03.png",
 		"assets/image/live/live_gauge_02_04.png",
 		"assets/image/live/live_gauge_02_05.png",
 		"assets/image/live/live_gauge_02_06.png",
-		"assets/image/live/hp_num.png",
 		-- effects
-		"assets/image/live/circleeffect.png", -- 20
+		"assets/image/live/circleeffect.png", -- 17
 		"assets/image/live/ef_308.png",
 		-- judgement
-		"assets/image/live/ef_313_004_w2x.png", -- 22
+		"assets/image/live/ef_313_004_w2x.png", -- 19
 		"assets/image/live/ef_313_003_w2x.png",
 		"assets/image/live/ef_313_002_w2x.png",
 		"assets/image/live/ef_313_001_w2x.png",
 		"assets/image/live/ef_313_000_w2x.png",
 		-- combo
-		"assets/image/live/combo/1.png", -- 28
+		"assets/image/live/combo/1.png", -- 24
 		"assets/image/live/combo/2.png",
 		"assets/image/live/combo/3.png",
 		"assets/image/live/combo/4.png",
@@ -57,10 +61,13 @@ function sifui:__construct()
 		"assets/image/live/combo/9.png",
 		"assets/image/live/combo/10.png",
 	}, {mipmaps = true})
+	while fontImageDataList:isComplete() == false do
+		async.wait()
+	end
 	-- fonts
-	self.scoreFont = love.graphics.newImageFont(self.images[11], "0123456789")
-	self.addScoreFont = love.graphics.newImageFont(self.images[12], "0123456789+")
-	self.staminaFont = love.graphics.newImageFont(self.images[19], "0123456789")
+	self.scoreFont = love.graphics.newImageFont(fontImageDataList:getValues(1), "0123456789")
+	self.addScoreFont = love.graphics.newImageFont(fontImageDataList:getValues(2), "0123456789+")
+	self.staminaFont = love.graphics.newImageFont(fontImageDataList:getValues(3), "0123456789")
 	-- quads
 	self.comboQuad = {
 		[0] = love.graphics.newQuad(0, 0, 48, 48, 240, 130),
