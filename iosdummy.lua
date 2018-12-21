8033db747004f1a1b6ef06920c9229f0de679cca
MikuAuahDark <auahdark687291@gmail.com>
2018-12-21 14:44:26 +0800

    Cleanup

--- iosdummy.lua
+++
@@ -1,6 +0,0 @@
--- Exit dummy entry point
--- Part of Live Simulator: 2
--- See copyright notice in main.lua
-
-local dummy = function() end
-return {Start = dummy, Update = dummy, Draw = dummy}, "iOS Dummy"
