b5315cba79dbbfe7fba5d9f45a01851b2e04aa12
Miku AuahDark <auahdark687291@gmail.com>
2019-08-30 09:20:15 +0800

    Simplify blur9 shader.

--- assets/shader/blur9.fs
+++ assets/shader/blur9.fs
@@ -2,19 +2,15 @@ extern vec2 resolution;
 extern vec2 dir;
 
 // https://github.com/Jam3/glsl-fast-gaussian-blur
-vec4 blur9(Image image, vec2 uv, vec2 direction) {
-	vec4 color = vec4(0.0);
-	vec2 off1 = vec2(1.3846153846) * direction;
-	vec2 off2 = vec2(3.2307692308) * direction;
-	color += texture2D(image, uv) * 0.2270270270;
-	color += texture2D(image, uv + (off1 / resolution)) * 0.3162162162;
-	color += texture2D(image, uv - (off1 / resolution)) * 0.3162162162;
-	color += texture2D(image, uv + (off2 / resolution)) * 0.0702702703;
-	color += texture2D(image, uv - (off2 / resolution)) * 0.0702702703;
-	return color;
-}
-
-vec4 effect(vec4 color, Image tex, vec2 tc, vec2 sc)
+vec4 effect(vec4 c, Image image, vec2 uv, vec2 sc)
 {
-	return blur9(tex, tc, dir) * color;
+	vec4 color = vec4(0.0);
+	vec2 off1 = vec2(1.3846153846) * dir;
+	vec2 off2 = vec2(3.2307692308) * dir;
+	color += Texel(image, uv) * 0.2270270270;
+	color += Texel(image, uv + (off1 / resolution)) * 0.3162162162;
+	color += Texel(image, uv - (off1 / resolution)) * 0.3162162162;
+	color += Texel(image, uv + (off2 / resolution)) * 0.0702702703;
+	color += Texel(image, uv - (off2 / resolution)) * 0.0702702703;
+	return color * c;
 }
