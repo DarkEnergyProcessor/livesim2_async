d8e16edec23fbc67cb96a8aeb3e4210e6f433cdd
Miku AuahDark <auahdark687291@gmail.com>
2019-12-17 19:26:15 +0800

    Color theme: use table instead of hardcode.

--- game/color_theme.lua
+++ game/color_theme.lua
@@ -3,7 +3,33 @@
 -- See copyright notice in main.lua
 
 local color = require("color")
-local colorTheme = {}
+local colorTheme = {
+	[1] = {
+		-- ff4fae
+		currentColor = {0xff, 0x4f, 0xae, color.hexFF4FAE},
+		-- ef46a1
+		currentColorDark = {0xef, 0x46, 0xa1, color.hexEF46A1},
+		-- c31c76
+		currentColorDarker = {0xc3, 0x1c, 0x76, color.hexC31C76}
+	},
+	[2] = {
+		-- 46baff
+		currentColor = {0x46, 0xba, 0xff, color.hex46BAFF},
+		-- 3bacf0
+		currentColorDark = {0x3b, 0xac, 0xf0, color.hex3BACF0},
+		-- 007ec6
+		currentColorDarker = {0x00, 0x7e, 0xc6, color.hex007EC6}
+	},
+	[3] = {
+		-- ffc22e
+		currentColor = {0xff, 0xc2, 0x2e, color.hexFFC22E},
+		-- e8b126
+		currentColorDark = {0xe8, 0xb1, 0x26, color.hexE8B126},
+		-- ac7b0a
+		currentColorDarker = {0xac, 0x7b, 0x0a, color.hexAC7B0A}
+	}
+}
+
 local currentColor, currentColorDark, currentColorDarker
 
 -- colid: 1 = μ's, 2 = Aqours, 3 = NijiGaku
@@ -13,30 +39,13 @@ function colorTheme.init(colid)
 end
 
 function colorTheme.set(colid)
-	if colid == 1 then
-		-- ff4fae
-		currentColor = {0xff, 0x4f, 0xae, color.hexFF4FAE}
-		-- ef46a1
-		currentColorDark = {0xef, 0x46, 0xa1, color.hexEF46A1}
-		-- c31c76
-		currentColorDarker = {0xc3, 0x1c, 0x76, color.hexC31C76}
-	elseif colid == 2 then
-		-- 46baff
-		currentColor = {0x46, 0xba, 0xff, color.hex46BAFF}
-		-- 3bacf0
-		currentColorDark = {0x3b, 0xac, 0xf0, color.hex3BACF0}
-		-- 007ec6
-		currentColorDarker = {0x00, 0x7e, 0xc6, color.hex007EC6}
-	elseif colid == 3 then
-		-- ffc22e
-		currentColor = {0xff, 0xc2, 0x2e, color.hexFFC22E}
-		-- e8b126
-		currentColorDark = {0xe8, 0xb1, 0x26, color.hexE8B126}
-		-- ac7b0a
-		currentColorDarker = {0xac, 0x7b, 0x0a, color.hexAC7B0A}
-	else
+	if colorTheme[colid] == nil then
 		error("unknown color id "..colid)
 	end
+
+	currentColor = colorTheme[colid].currentColor
+	currentColorDark = colorTheme[colid].currentColorDark
+	currentColorDarker = colorTheme[colid].currentColorDarker
 end
 
 function colorTheme.get(opacity)
