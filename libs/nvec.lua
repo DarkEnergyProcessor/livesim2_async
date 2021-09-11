c7c2a2c470d468d0b201eedc4b5620bfa4dcfb1d
Miku AuahDark <lumi.hotaru@gmail.com>
2021-09-11 20:10:59 +0800

    Update NVec.

--- libs/nvec.lua
+++ libs/nvec.lua
@@ -24,10 +24,12 @@
 
 local type = type
 local sin, cos, atan2 = math.sin, math.cos, (math.atan2 or math.atan)
-local sqrt, rnd = math.sqrt, math.random
+local sqrt, rnd, pi = math.sqrt, math.random, math.pi
 local ffi = nil
 
-local nvec, nvec_t = {}, nil
+---@class NVec
+local nvec = {}
+local nvec_t = nil
 local __construct, isVector
 
 if rawget(_G, "jit") and jit.status() and package.preload.ffi then
@@ -53,106 +55,145 @@ else
 	end
 end
 
+---@param angle number
+---@param radius number
+---@return NVec
 function nvec.fromPolar(angle, radius)
 	radius = radius or 1
 	return __construct(cos(angle) * radius, sin(angle) * radius)
 end
 
+---@param min number
+---@param max number
+---@return NVec
 function nvec.randomDirection(min, max)
 	min = min or 1
 	max = max or min
 	assert(max > 0, "max length must be greater than zero")
 	assert(max >= min, "max length must be greater than or equal to min length")
-	return nvec.fromPolar(rnd() * 2*math.pi, rnd() * (max - min) + min)
+	return nvec.fromPolar(rnd() * 2*pi, rnd() * (max - min) + min)
 end
 
+---@return NVec
 function nvec:clone()
 	return __construct(self.x, self.y)
 end
 
+---@return number,number
 function nvec:unpack()
 	return self.x, self.y
 end
 
+---@return string
 function nvec:__tostring()
 	-- prefer string.format over concatenation for
 	-- optimization purpose in LuaJIT 2.1
 	return string.format("(%.14g,%.14g)", self:unpack())
 end
 
+---@return NVec
 function nvec:__unm()
 	return __construct(-self.x, -self.y)
 end
 
-function nvec.__add(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	return __construct(a.x + b.x, a.y + b.y)
+---@param b NVec
+---@return NVec
+function nvec:__add(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	return __construct(self.x + b.x, self.y + b.y)
 end
 
-function nvec.__sub(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	return __construct(a.x - b.x, a.y - b.y)
+---@param b NVec
+---@return NVec
+function nvec:__sub(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	return __construct(self.x - b.x, self.y - b.y)
 end
 
-function nvec.__mul(a, b)
-	if type(a) == "number" then
-		return __construct(b.x * a, b.y * a)
+---@param b number|NVec
+---@return number|NVec
+function nvec:__mul(b)
+	if type(self) == "number" then
+		return __construct(b.x * self, b.y * self)
 	elseif type(b) == "number" then
-		return __construct(a.x * b, a.y * b)
+		return __construct(self.x * b, self.y * b)
 	else
-		assert(isVector(a) and isVector(b), "NVec expected")
-		return a.x * b.x + a.y * b.y
+		assert(isVector(self) and isVector(b), "NVec expected")
+		return self.x * b.x + self.y * b.y
 	end
 end
 
-function nvec.__div(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	return __construct(a.x / b.x, a.y / b.y)
+---@param b NVec
+---@return NVec
+function nvec:__div(b)
+	assert(isVector(self), "NVec expected")
+
+	if isVector(b) then
+		return __construct(self.x / b.x, self.y / b.y)
+	else
+		return __construct(self.x / b, self.y / b)
+	end
 end
 
-function nvec.__eq(a, b)
-	return a.x == b.x and a.y == b.y
+---@param b NVec
+function nvec:__eq(b)
+	return isVector(self) and isVector(b) and self.x == b.x and self.y == b.y
 end
 
-function nvec.__lt(a, b)
-	return a.x < b.x or (a.x == b.x and a.y < b.y)
+---@param b NVec
+function nvec:__lt(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	return self.x < b.x or (self.x == b.x and self.y < b.y)
 end
 
-function nvec.__le(a, b)
-	return a.x <= b.x and a.y <= b.y
+---@param b NVec
+function nvec:__le(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	return self.x <= b.x and self.y <= b.y
 end
 
-function nvec.dot(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	return a.x * b.x + a.y * b.y
+---@param b NVec
+---@return number
+function nvec:dot(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	return self.x * b.x + self.y * b.y
 end
 
-function nvec.permul(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	return __construct(a.x * b.x, a.y * b.y)
+---@param b NVec
+---@return NVec
+function nvec:permul(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	return __construct(self.x * b.x, self.y * b.y)
 end
 
+---@return NVec
 function nvec:toPolar()
 	return __construct(atan2(self.x, self.y), self:len())
 end
 
+---@return number
 function nvec:len2()
 	return self.x * self.x + self.y * self.y
 end
 
+---@return number
 function nvec:len()
 	return sqrt(self.x * self.x + self.y * self.y)
 end
 
-function nvec.dist2(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	local c, d = a.x - b.x, a.y - b.y
+---@param b NVec
+---@return number
+function nvec:dist2(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	local c, d = self.x - b.x, self.y - b.y
 	return c * c + d * d
 end
 
-function nvec.dist(a, b)
-	assert(isVector(a) and isVector(b), "NVec expected")
-	local c, d = a.x - b.x, a.y - b.y
+---@param b NVec
+---@return number
+function nvec:dist(b)
+	assert(isVector(self) and isVector(b), "NVec expected")
+	local c, d = self.x - b.x, self.y - b.y
 	return sqrt(c * c + d * d)
 end
 
@@ -169,21 +210,26 @@ function nvec:normalized()
 end
 nvec.normalize = nvec.normalized
 
+---@param angle number
 function nvec:rotateInplace(angle)
 	local c, s = cos(angle), sin(angle)
 	self.x, self.y = c * self.x - s * self.y, s * self.x + c * self.y
 	return self
 end
 
+---@param angle number
 function nvec:rotate(angle)
 	return self:clone():rotateInplace(angle)
 end
 nvec.rotated = nvec.rotate
 
+---@return NVec
 function nvec:perpendicular()
 	return __construct(-self.y, self.x)
 end
 
+---@param v NVec
+---@return NVec
 function nvec:projectOn(v)
 	assert(isVector(v), "NVec expected")
 	-- (self * v) * v / v:len2()
@@ -191,6 +237,8 @@ function nvec:projectOn(v)
 	return __construct(s * v.x, s * v.y)
 end
 
+---@param v NVec
+---@return NVec
 function nvec:mirrorOn(v)
 	assert(isVector(v), "NVec expected")
 	-- 2 * self:projectOn(v) - self
@@ -198,11 +246,14 @@ function nvec:mirrorOn(v)
 	return __construct(s * v.x - self.x, s * v.y - self.y)
 end
 
+---@param v NVec
+---@return NVec
 function nvec:cross(v)
 	assert(isVector(v), "NVec expected")
 	return self.x * v.y - self.y * v.x
 end
 
+---@param maxLen number
 function nvec:trimInplace(maxLen)
 	local s = maxLen * maxLen / (self.x * self.x + self.y * self.y)
 	s = (s > 1 and 1) or sqrt(s)
@@ -210,11 +261,14 @@ function nvec:trimInplace(maxLen)
 	return self
 end
 
+---@param maxLen number
 function nvec:trim(maxLen)
 	return self:clone():trimInplace(maxLen)
 end
-nvec.trimmed = nvec.trin
+nvec.trimmed = nvec.trim
 
+---@param other NVec|nil
+---@return number
 function nvec:angleTo(other)
 	if other then
 		return atan2(self.y, self.x) - atan2(other.y, other.x)
@@ -222,11 +276,28 @@ function nvec:angleTo(other)
 	return atan2(self.y, self.x)
 end
 
+
+---@type fun(x: number, y: number): NVec
 nvec.new = __construct
+nvec.is = isVector
 nvec.__index = nvec
 
--- FFI metatype set for FFI accelerated version
 if nvec_t then
+	-- Make sure NVec is __pairs compatible
+	-- https://github.com/MikuAuahDark/NPad93/pull/2
+	local function iter(self, v)
+		if v == nil then
+			return "x", self.x
+		elseif v == "x" then
+			return "y", self.y
+		end
+	end
+
+	function nvec:__pairs()
+		return iter, self, nil
+	end
+
+	-- FFI metatype set for FFI accelerated version
 	ffi.metatype(nvec_t, nvec)
 end
 
